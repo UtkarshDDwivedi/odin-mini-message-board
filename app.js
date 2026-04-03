1693133df0ad44b926ae7633e32a7648f89dbc8a
utkarsh <utkarshdhar05@gmail.com>
2026-04-03 23:48:40 +0530

    add new message

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const assetPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(assetPath))
+
+app.use(express.static(assetPath));
+app.use(express.urlencoded({extended: true}));
 
 
 messages = [
@@ -26,6 +28,15 @@ messages = [
 
 app.get('/', (req, res) => {
     res.render("index", {title: "Mini Message Board", messages: messages});
+})
+
+app.get('/new', (req, res) => {
+    res.render("form", {title: "Mini Message Board"});
+})
+
+app.post('/new', (req, res) => {
+    messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+    res.redirect('/');
 })
 
 app.listen(port, () => {
